437aa22c627ded41339e976c349eab9b4122c8b7
liuyi <devliuyi@gmail.com>
2019-02-04 09:25:03 +0800

    Enable content hash.

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,15 @@ const webpack = require('webpack');
 
 module.exports = {
     configureWebpack: {
+        output: {
+            filename: '[name].[contenthash].js',
+            chunkFilename: '[name].[contenthash].js'
+        },
         plugins: [
             new webpack.HashedModuleIdsPlugin()
-        ]
+        ],
+        optimization: {
+            runtimeChunk: 'single'
+        }
     }
 }
